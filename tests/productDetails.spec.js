791605810c263cfea26e8042a3b6cff0e4a71d9a
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-09-21 18:20:29 -0300

    requisito 7 terminado

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,28 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    const product = productDetails('Alcool gel', 'Máscara');
     // ESCREVA SEUS TESTES ABAIXO:
+
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
+
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(product)).toBeTruthy();
+
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(product.length).toBe(2);
+
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('Álcool', 'luvas')[0]).toBe('object');
+    expect(typeof productDetails('Álcool', 'luvas')[1]).toBe('object');
+
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    const prod = productDetails('Carro', 'Notebook');
+    expect(prod[0]).not.toEqual(prod[1]);
+
     // Teste se os dois productIds terminam com 123.
+    expect(product[0].details.productId.slice(-3)).toBe('123');
+    expect(product[1].details.productId.slice(-3)).toBe('123');
   });
 });
